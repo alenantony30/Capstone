33563462bec3e0ccb47e83f61c815562988ad167
alenantony30 <alenantony30@gmail.com>
2023-04-27 18:09:22 +0530

    test

--- src/main/java/com/capstone/employee/EmployeeManagementSystemApplication.java
+++ src/main/java/com/capstone/employee/EmployeeManagementSystemApplication.java
@@ -3,9 +3,6 @@ package com.capstone.employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.capstone.employee.service.EmployeeService;
-import com.capstone.employee.service.IEmployeeService;
-
 @SpringBootApplication
 public class EmployeeManagementSystemApplication {
 
@@ -14,9 +11,7 @@ public class EmployeeManagementSystemApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
-		IEmployeeService employeeService=new EmployeeService();
-		System.out.println(employeeService.getEmployees());
-		
+	
 	}
 
 }
