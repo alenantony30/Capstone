e894a954b22a66bae91058cc2e8249e6e42be58e
alenantony30 <alenantony30@gmail.com>
2023-04-27 18:52:25 +0530

    test

--- src/main/java/com/capstone/employee/EmployeeManagementSystemApplication.java
+++ src/main/java/com/capstone/employee/EmployeeManagementSystemApplication.java
@@ -11,6 +11,7 @@ public class EmployeeManagementSystemApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
+		System.out.println("Hello World");
 	
 	}
 
